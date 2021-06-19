cb471f5f15dc63d01d87738bb466d87c6058845d
oyamabilana <oyamabilana5@gmail.com>
2021-06-19 12:34:30 +0200

    Updated API methods

--- parcel_service/app.js
+++ parcel_service/app.js
@@ -4,9 +4,7 @@ const bodyparser = require('body-parser');
 const route = require('./route');
 const app = express();
 
-app.use(bodyparser.json({limit: '100mb'}));
-app.use(bodyparser.urlencoded({limit: '50mb', extended: true, parameterLimit: 5000}));
-app.use(route); 
-app.use('/', (req, res,next)=>{});
+app.use(express.json({limit: '50mb'}));
+app.use('/api/parcel/service',route); 
 const server = http.createServer(app);
 server.listen(3001);
--- parcel_service/route.js
+++ parcel_service/route.js
@@ -3,19 +3,9 @@ const parcel = require('./modules/parcel');
 const router = express.Router();
 //const mssqlcon = require ('./modules/dbconnection');
 
-router.get('/parcel', parcel.getAllParcels );
-router.post('/parcel', parcel.addParcel );
-router.get('/parcel/id', parcel.getParcelById);
-router.get('/parcel/code', parcel.getParcelByCode);
-
-/** 
-router.get('/search', async(req, res) =>{
-    const conn = await mssqlcon.getConnection();
-    const result = await conn.request()
-        .input('parcel_id', req.query.id)
-        .execute('getParcelById')
-    res.send(result)
-})
-*/
+router.get('/getAllParcels', parcel.getAllParcels );
+router.post('/addParcel', parcel.addParcel );
+router.get('/getParcelById', parcel.getParcelById);
+router.get('/getParcelByCode', parcel.getParcelByCode);
 
 module.exports = router;
